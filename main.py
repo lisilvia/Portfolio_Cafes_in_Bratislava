9748e35ffc547689767333eb84122b4e9bd59d27
Kor1 <prkladprkldn@gmail.com>
2023-06-27 17:12:42 +0200

    Change database

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ csrf = CSRFProtect(app)
 Bootstrap(app)
 
 ##Connect to Database
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///cafes.db"
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+#app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///cafes.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
